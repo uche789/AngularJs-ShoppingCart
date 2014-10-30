7933b3209220416d860a961871a9b605fadf8d9e
uche789 <uche.edozien@yahoo.com>
2014-10-30 14:34:23 -0230

    Deleted useless files

--- app/js/phoneList.js
+++
@@ -1,8 +0,0 @@
-// JavaScript source code
-
-var phoneListApp = angular.module('phoneList', []);
-
-phoneListApp.controller('phoneListCrtl', ['$scope', function ($scope) {
-        $scope.CurrentPhoneNumber = '77-88-8889';
-    }
-]);
